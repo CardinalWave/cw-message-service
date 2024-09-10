0b9ac65b9caf9d4dedd0798128a2e334ee20dc0d
vmarquezv <vmarquezv@debian>
2024-09-10 20:42:08 -0300

    refact: change mqtt connectio

--- src/main/mqtt/mqtt_conn.py
+++ src/main/mqtt/mqtt_conn.py
@@ -1,3 +1,4 @@
+import time
 import paho.mqtt.client as mqtt
 
 
@@ -7,13 +8,29 @@ class MQTTClient:
         self.broker_port = port
         self.client = mqtt.Client()
         self.client_connect()
+        self.client.loop_start()
 
     def client_connect(self):
         self.client.connect(self.broker_ip, self.broker_port)
 
     def publish_message(self, topic, message):
-
-        self.client.publish(topic, message)
+        retries = 0
+        max_retries = 3
+        retry_delay = 5
+        while retries < max_retries:
+            result = self.client.publish(topic, message)
+            if result.rc == mqtt.MQTT_ERR_SUCCESS:
+                print(f"Success: {topic} - {message}")
+                return True
+            else:
+                self.client_connect()
+                print(f"Failed - {result.rc} - Tryin:  {retries + 1}")
+                retries += 1
+                if retries < max_retries:
+                    self.client.reconnect()
+                    time.sleep(retry_delay)
+        print(f"Failed to publish - {topic} : {message}")
+        return False
 
     def close_connection(self):
         self.client.disconnect()
